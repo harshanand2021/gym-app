b11d5bd992565f8197700336c97779c23aa24978
Harsh Anand <harsh250600@gmail.com>
2024-10-25 14:54:13 +0530

    Hero component modified

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="hero">
+      <div className="content">
+        <div className="title">
+          <h1>LET'S</h1>
+          <h1>GET</h1>
+          <h1>MOVING</h1>
+        </div>
+        <div className="sub-title">
+          <p>Your journey to fitness starts here</p>
+          <p>Unleash your potential</p>
+        </div>
+        <div className="buttons">
+          <button>Start Your Journey</button>
+          <button>Discover Your Plan</button>
+        </div>
+      </div>
+    </section>
+  );
 }
