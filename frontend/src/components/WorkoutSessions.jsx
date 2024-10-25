abe1f1dc6511d3406bdb94735a96fa00bdb36892
Harsh Anand <harsh250600@gmail.com>
2024-10-25 15:06:43 +0530

    WorkoutSession component modified and styled

--- frontend/src/components/WorkoutSessions.jsx
+++ frontend/src/components/WorkoutSessions.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
 
 export default function WorkoutSessions() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          repudiandae expedita autem!
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga a atque
+          iure earum molestias minima aliquam nostrum doloribus, quae amet?
+          Laborum, pariatur!
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              saepe repellendus nemo sit facere ipsam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
